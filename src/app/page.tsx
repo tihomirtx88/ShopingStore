80dcef9f563ed7e849f799b6bec3ebdf5f9d5f76
tihomirtx88 <tihomirtx88@gmail.com>
2025-05-08 21:15:21 +0300

    Setup again next.js project and setup nesesery components from shadcn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+
 function HomePage() {
   return (
     <>
       <div>Home page</div>
+      <Button variant='outline' size='lg' className="capitalize m-8 text-red-500">Add</Button>
     </>
   );
 }
