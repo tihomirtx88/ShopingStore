74f4977362f66cf19cce0d00a4fa6419fdb6c515
tihomirtx88 <tihomirtx88@gmail.com>
2025-05-11 16:46:04 +0300

    Implement logo component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono, Inter } from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/navbar/Navbar";
 import Container from "@/components/global/Container";
--- src/components/navbar/Logo.tsx
+++ src/components/navbar/Logo.tsx
@@ -1,5 +1,13 @@
+import Link from "next/link";
+import { Button } from "../ui/button";
+import { FaHome } from "react-icons/fa";
+
 export default function Logo() {
   return (
-    <div>Logo</div>
-  )
+    <Button size='icon' asChild>
+      <Link href="/">
+        <FaHome className="w-6 h-6"/>
+      </Link>
+    </Button>
+  );
 }
