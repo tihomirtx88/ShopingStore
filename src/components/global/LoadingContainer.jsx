3e0f83b69eedce9cbdf7050d012c40e4b9ad210e
tihomirtx88 <tihomirtx88@gmail.com>
2025-05-12 15:58:04 +0300

    Adding more compoonents

---
+++ src/components/global/LoadingContainer.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function LoadingContainer() {
+  return <div>LoadingContainer</div>;
+}
---
+++ src/components/home/HeroCarousel.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function HeroCarousel() {
+  return <div>HeroCarousel</div>;
+}
