0fd64718fccb2aa5895f3f2c2762ed9fc342abbc
tihomirtx88 <tihomirtx88@gmail.com>
2025-05-14 10:22:58 +0300

    Fetch products functionalioty

--- src/components/home/FeaturedProducts.jsx
+++ src/components/home/FeaturedProducts.jsx
@@ -1,5 +1,17 @@
 import React from "react";
+import { fetchFeaturedProducts } from "../../../utils/actions";
+import EmptyList from "../global/EmptyList";
+import SectionTitle from "../global/SectionTitle";
+import ProductsGrid from "../products/ProductsGrid";
 
-export default function FeaturedProducts() {
-  return <div>FeaturedProducts</div>;
+export default async function FeaturedProducts() {
+  const porducts = await fetchFeaturedProducts();
+  
+  if(porducts.length === 0) return <EmptyList/>;
+  return (
+    <section className="pt-24">
+      <SectionTitle text="featured products"/>
+      <ProductsGrid products={products}/>
+    </section>
+  );
 }
