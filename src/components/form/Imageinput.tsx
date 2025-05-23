8c424044d441e54f4d76cf01bfcc749b13643af1
tihomirtx88 <tihomirtx88@gmail.com>
2025-05-23 09:43:44 +0300

    Implement PriceInput and ImageInput components

--- src/components/form/Imageinput.tsx
+++ src/components/form/Imageinput.tsx
@@ -1,5 +1,16 @@
 import React from "react";
+import { Label } from "../ui/label";
+import { Input } from "../ui/input";
 
 export default function Imageinput() {
-  return <div>Imageinput</div>;
+   const name = 'image';
+
+  return (
+    <div className='mb-2'>
+      <Label htmlFor={name} className='capitalize'>
+        Image
+      </Label>
+      <Input id={name} name={name} type='file' required accept='image/*' />
+    </div>
+  );
 }
--- src/components/form/PriceInput.tsx
+++ src/components/form/PriceInput.tsx
@@ -1,3 +1,25 @@
-export default function PriceInput() {
-  return <div>PriceInput</div>;
+import { Input } from "../ui/input";
+import { Label } from "../ui/label";
+
+const name = 'price';
+type FormInputNumberProps = {
+  defaultValue?: number;
+};
+
+export default function PriceInput({defaultValue}: FormInputNumberProps) {
+   return (
+    <div className='mb-2'>
+      <Label htmlFor={name} className='capitalize'>
+        Price ($)
+      </Label>
+      <Input
+        id={name}
+        type='number'
+        name={name}
+        min={0}
+        defaultValue={defaultValue || 100}
+        required
+      />
+    </div>
+  );
 }
