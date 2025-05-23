925884c366b4fb7af183eaa7ab456bb0b31e0a13
tihomirtx88 <tihomirtx88@gmail.com>
2025-05-23 09:48:03 +0300

    Implement TeaxtAreaInput and CheckBoxInput components

--- src/components/form/TextArea.tsx
+++ src/components/form/TextArea.tsx
@@ -1,3 +1,26 @@
-export default function TextArea() {
-  return <div>TextArea</div>;
+import { Label } from "../ui/label";
+import { Textarea } from "../ui/textarea";
+
+type TextAreaInputProps = {
+  name: string;
+  labelText?: string;
+  defaultValue?: string;
+};
+
+export default function TextArea({ name, labelText, defaultValue }: TextAreaInputProps) {
+   return (
+    <div className='mb-2'>
+      <Label htmlFor={name} className='capitalize'>
+        {labelText || name}
+      </Label>
+      <Textarea
+        id={name}
+        name={name}
+        defaultValue={defaultValue}
+        rows={5}
+        required
+        className='leading-loose'
+      />
+    </div>
+  );
 }
