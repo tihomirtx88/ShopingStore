0f9244320862a817a75eebed6225d830f7d9ee41
tihomirtx88 <tihomirtx88@gmail.com>
2025-05-24 22:49:43 +0300

    Implement form container and create product action

--- src/components/form/FormContainer.tsx
+++ src/components/form/FormContainer.tsx
@@ -1,3 +1,23 @@
-export default function FormContainer() {
-  return <div>FormContainer</div>;
+'use client';
+import { useFormState } from "react-dom";
+import { actionFunction } from "../../../utils/types";
+import { toast } from "sonner";
+import { useEffect } from "react";
+
+const initialState = {
+  message: '',
+};
+
+export default function FormContainer({ action, children,}: { action: actionFunction; children: React.ReactNode;}) {
+   const [state, formAction] = useFormState(action, initialState);
+
+    useEffect(() => {
+    if (state.message) {
+      toast("Notification", {
+      description: state.message,
+    });
+    }
+  }, [state]);
+
+  return <form action={formAction}>{children}</form>;
 }
