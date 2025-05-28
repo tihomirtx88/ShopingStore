b4e856c0e6a863d5495bf29e78e2d34a3648f594
tihomirtx88 <tihomirtx88@gmail.com>
2025-05-28 21:04:56 +0300

    Fixing-image-product-bug

--- next.config.ts
+++ next.config.ts
@@ -2,14 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  images:{
-    remotePatterns:[
+ images: {
+    remotePatterns: [
       {
-        protocol: 'https', 
-        hostname: 'images.pexels.com'
-      }
-    ]
-  }
+        protocol: 'https',
+        hostname: 'images.pexels.com',
+      },
+    ],
+    domains: ['pkhrcwdgwgoenfdvbicg.supabase.co'],  
+  },
 };
 
 export default nextConfig;
--- src/components/products/ProductsGrid.tsx
+++ src/components/products/ProductsGrid.tsx
@@ -22,24 +22,34 @@ export default function ProductsGrid({ products }: { products: Product[] }) {
         const { name, price, image } = product;
         const productId = product.id;
         const dollarsAmount = formatCurrency(price);
+        
         return (
           <article key={productId} className="group relative">
             <Link href={`/products/${productId}`}>
               <Card className="border-none transform group-hover:shadow-xl transition-shadow duration-500">
                 <CardContent className="p-4">
                   <div className="relative h-64 md:h-48 rounded overflow-hidden">
-                    <Image
-                      src={image}
-                      alt={name}
-                      fill
-                      sizes='(max-width:768px) 100vw,(max-width:1200px) 50vw, 33vw'
-                      priority
-                      className="rounded w-full object-cover transform group-hover:scale-110 transition-transform duration-500"
-                    />
+                    {image ? (
+                      <Image
+                        src={image}
+                        alt={name}
+                        fill
+                        sizes="(max-width:768px) 100vw,(max-width:1200px) 50vw, 33vw"
+                        priority
+                        className="rounded w-full object-cover transform group-hover:scale-110 transition-transform duration-500"
+                      />
+                    ) : (
+                      <div className="bg-gray-200 w-full h-full rounded" />
+                    )}
                   </div>
                   <div className="mt-4 text-center">
                     <h3 className="text-lg capitalize">{name}</h3>
-                    <p className="text-muted-foreground mt-2" style={{color: "hsl(var(--muted-foreground))"}}>{dollarsAmount}</p>
+                    <p
+                      className="text-muted-foreground mt-2"
+                      style={{ color: "hsl(var(--muted-foreground))" }}
+                    >
+                      {dollarsAmount}
+                    </p>
                   </div>
                 </CardContent>
               </Card>
