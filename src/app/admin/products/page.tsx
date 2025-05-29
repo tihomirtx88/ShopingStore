0b138b48c05a8e6620907c8e08bce613ff924bca
tihomirtx88 <tihomirtx88@gmail.com>
2025-05-29 20:09:51 +0300

    Render admin porducts

--- src/app/admin/products/page.tsx
+++ src/app/admin/products/page.tsx
@@ -1,8 +1,53 @@
+import EmptyList from "@/components/global/EmptyList";
+import { fetchAdminProducts } from "../../../../utils/actions";
+import { Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
+import Link from "next/link";
+import { formatCurrency } from "../../../../utils/format";
 
 
-export default function AdminProductPage() {
+export default async function AdminProductPage() {
+  const items = await fetchAdminProducts();
+  if (items.length === 0) return <EmptyList />;
 
   return (
-    <div>ProductPage</div>
+    <section>
+      <Table>
+         <TableCaption className='capitalize'>
+          total products : {items.length}
+        </TableCaption>
+         <TableHeader>
+          <TableRow>
+            <TableHead>Product Name</TableHead>
+            <TableHead>Company</TableHead>
+            <TableHead>Price</TableHead>
+            <TableHead>Actions</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {items.map((item) => {
+            const { id: productId, name, company, price } = item;
+            return (
+              <TableRow key={productId}>
+                <TableCell>
+                  <Link
+                    href={`/products/${productId}`}
+                    className='underline text-muted-foreground tracking-wide capitalize'
+                  >
+                    {name}
+                  </Link>
+                </TableCell>
+                <TableCell>{company}</TableCell>
+                <TableCell>{formatCurrency(price)}</TableCell>
+                <TableCell className='flex items-center gap-x-2'>
+                  <Link href={`/admin/products/${productId}/edit`}>
+                  Edit
+                  </Link>
+                </TableCell>
+              </TableRow>
+            );
+          })}
+        </TableBody>
+      </Table>
+    </section>
   )
 }
