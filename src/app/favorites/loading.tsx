b3d5fd10e7877801f02b2f0d451e3fa3fba0122f
tihomirtx88 <tihomirtx88@gmail.com>
2025-06-17 17:51:39 +0300

    Implement- favroite page and action for it

---
+++ src/app/favorites/loading.tsx
@@ -0,0 +1,9 @@
+'use client'
+
+import LoadingContainer from "@/components/global/LoadingContainer"
+
+export default function loading() {
+  return (
+    <LoadingContainer/>
+  )
+}
--- src/app/favorites/page.jsx
+++ src/app/favorites/page.jsx
@@ -1,6 +1,15 @@
-function Favorites() {
-    return (
-     <div>Admin page</div>
-    );
-  }
-  export default Favorites;
+import SectionTitle from "@/components/global/SectionTitle";
+import { fetchUserFavroite } from "../../../utils/actions";
+import ProductsGrid from "@/components/products/ProductsGrid";
+
+async function FavoritesPage() {
+  const favorites = await fetchUserFavroite();
+  if(favorites.length === 0) return <SectionTitle text="You not have favroites yet"/>
+  return (
+    <div>
+      <SectionTitle text="Favroites"/>
+      <ProductsGrid products={favorites.map((favorite) => favorite.productid)}/>
+    </div>
+  );
+}
+export default FavoritesPage;
