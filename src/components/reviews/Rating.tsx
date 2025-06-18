7c73f71616f46ff4e24333e76cce4dc2b8ff85dc
tihomirtx88 <tihomirtx88@gmail.com>
2025-06-18 14:06:59 +0300

    Implement raitng component

--- src/components/reviews/Rating.tsx
+++ src/components/reviews/Rating.tsx
@@ -1,7 +1,25 @@
-import React from 'react'
+import { FaStar, FaRegStar } from "react-icons/fa";
 
-export default function Rating() {
-  return (
-    <div>Rating</div>
-  )
+export default function Rating({ rating }: { rating: number }) {
+  // rating = 2
+  // 1 <= 2 true
+  // 2 <= 2 true
+  // 3 <= 2 false
+  // 4 <= 2 false
+  // 5 <= 2 false
+  const stars = Array.from({ length: 5 }, (_, i) => i + 1 <= rating);
+   return (
+    <div className='flex items-center gap-x-1'>
+      {stars.map((isFilled, i) => {
+        const className = `w-3 h-3 ${
+          isFilled ? 'text-primary' : 'text-grey-400'
+        }`;
+        return isFilled ? (
+          <FaStar className={className} key={i} />
+        ) : (
+          <FaRegStar className={className} key={i} />
+        );
+      })}
+    </div>
+  );
 }
