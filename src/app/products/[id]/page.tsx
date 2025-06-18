ce0e5f15c746fa34951f1fcdaac38ea7f86cd813
tihomirtx88 <tihomirtx88@gmail.com>
2025-06-18 09:46:51 +0300

    Implement submitReview component

--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -6,6 +6,8 @@ import FavoriteToggleButton from "@/components/products/FavoriteToggleButton";
 import ProductRating from "@/components/single-product/ProductRating";
 import AddtoCart from "@/components/single-product/AddtoCart";
 import ShareButton from "@/components/single-product/ShareButton";
+import SubmitReview from "@/components/reviews/SubmitReview";
+// import ProductReviews from "@/components/reviews/ProductReviews";
 
 
 type Props = {
@@ -19,6 +21,7 @@ export default async function SingleProductPage(props: Props) {
   const product = await fetchsingleProduct(id);
   const { name, company, price, image, description } = product;
   const dollarsAmount = formatCurrency(price);
+
   return <section>
     <BreadCrumbps name={product.name}/>
     <div className="mt-6 grid gap-y-8 lg:grid-cols-2 lg:gap-x-16">
@@ -44,5 +47,7 @@ export default async function SingleProductPage(props: Props) {
           <AddtoCart productId={id} />
         </div>
     </div>
+    {/* <ProductReviews productId={id}/> */}
+    <SubmitReview productId={id}/>
   </section>;
 }
--- src/components/reviews/SubmitReview.tsx
+++ src/components/reviews/SubmitReview.tsx
@@ -1,7 +1,47 @@
-import React from 'react'
+"use client";
 
-export default function SubmitReview() {
+import { useUser } from "@clerk/nextjs";
+import { useState } from "react";
+import { Button } from "../ui/button";
+import { Card } from "../ui/card";
+import FormContainer from "../form/FormContainer";
+import { createReview } from "../../../utils/actions";
+import RatingInput from "./RatingInput";
+import TextArea from "../form/TextArea";
+import Buttons from "../form/Buttons";
+
+export default function SubmitReview({ productId }: { productId: string }) {
+  const [isReviewFormVisible, setIsReviewFormVisible] = useState(false);
+  const { user } = useUser();
   return (
-    <div>SubmitReview</div>
-  )
+    <div>
+      <Button
+        size="lg"
+        className="capitalize"
+        onClick={() => setIsReviewFormVisible((prev) => !prev)}
+      >
+        leave review
+      </Button>
+      {isReviewFormVisible && (
+        <Card className='p-8 mt-8'>
+            <FormContainer action={createReview}>
+               <input type='hidden' name='productId' value={productId} />
+               <input
+                    type='hidden'
+                    name='authorName'
+                    value={user?.firstName || 'user'}
+                />
+                <input type='hidden' name='authorImageUrl' value={user?.imageUrl} />
+                <RatingInput name='rating' />
+                <TextArea
+                    name='comment'
+                    labelText='feedback'
+                    defaultValue='Outstanding product!!!'
+                />
+                <Buttons className='mt-4' />
+            </FormContainer>
+        </Card>
+      )}
+    </div>
+  );
 }
