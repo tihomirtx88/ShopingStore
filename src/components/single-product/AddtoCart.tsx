4a2c10bc7cc27328c6e14301d95b38ca6c601158
tihomirtx88 <tihomirtx88@gmail.com>
2025-06-27 12:48:21 +0300

    Implement ad to cart component

--- src/components/single-product/AddtoCart.tsx
+++ src/components/single-product/AddtoCart.tsx
@@ -1,11 +1,33 @@
-import React from "react";
-import { Button } from "../ui/button";
+'use client';
 
-export default function AddtoCart({productId}:{productId: string}) {
-console.log(productId);
+import { useState } from "react";
+import { useAuth } from "@clerk/nextjs";
+import SelectProductAmount, { Mode } from "./SelectProductamount";
+import Buttons, { ProductSignInButton } from "../form/Buttons";
+import FormContainer from "../form/FormContainer";
+import { addToCartAction } from "../../../utils/cart";
 
-  
-  return <Button className="capitalize mt-8 bg-blue-500 hover:bg-blue-100 cursor-pointer" size='lg'>
-    Add To Cart
-  </Button>
+// import { useState } from 'react';
+// import SelectProductAmount from './SelectProductAmount';
+// import { Mode } from './SelectProductAmount';
+// import FormContainer from '../form/FormContainer';
+// import { SubmitButton } from '../form/Buttons';
+// import { addToCartAction } from '@/utils/actions';
+// import { useAuth } from '@clerk/nextjs';
+// import { ProductSignInButton } from '../form/Buttons';
+
+export default function AddtoCart({ productId }: { productId: string }) {
+  const [amount, setAmount] = useState(1);
+  const { userId } = useAuth();
+
+  return (
+    <div className="mt-4">
+      <SelectProductAmount mode={Mode.SingleProduct} amount={amount} setAmount={setAmount}/>
+      {userId ? <FormContainer action={addToCartAction}>
+        <input type="hidden" name="productId" value={productId}/>
+        <input type="hidden" name="amount" value={amount}/>
+        <Buttons text='add to cart' className="mt-8"/>
+      </FormContainer> : <ProductSignInButton/>}
+    </div>
+  );
 }
