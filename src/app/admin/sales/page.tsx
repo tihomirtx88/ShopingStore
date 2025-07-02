60ecad632ac5472958e1830b1e6be268f1d0aa5f
tihomirtx88 <tihomirtx88@gmail.com>
2025-07-02 11:37:01 +0300

    Implement sales page

--- src/app/admin/sales/page.tsx
+++ src/app/admin/sales/page.tsx
@@ -1,7 +1,46 @@
-import React from 'react'
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { fetchAdminOrders } from "../../../../utils/order";
+import { formatCurrency, formatDate } from "../../../../utils/format";
 
-export default function page() {
-    return (
-        <div>page</div>
-    )
+export default async function page() {
+  const orders = await fetchAdminOrders();
+  return (
+    <Table>
+      <TableCaption>Total Orders : {orders.length}</TableCaption>
+      <TableHeader>
+        <TableRow>
+          <TableHead>Email</TableHead>
+          <TableHead>Products</TableHead>
+          <TableHead>Order Total</TableHead>
+          <TableHead>Tax</TableHead>
+          <TableHead>Shipping</TableHead>
+          <TableHead>Date</TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        {orders.map((order) => {
+          const { products, orderTotal, tax, shipping, createdAt, email } =
+            order;
+          return (
+            <TableRow key={order.id}>
+              <TableCell>{email}</TableCell>
+              <TableCell>{products}</TableCell>
+              <TableCell>{formatCurrency(orderTotal)}</TableCell>
+              <TableCell>{formatCurrency(tax)}</TableCell>
+              <TableCell>{formatCurrency(shipping)}</TableCell>
+              <TableCell>{formatDate(createdAt)}</TableCell>
+            </TableRow>
+          );
+        })}
+      </TableBody>
+    </Table>
+  );
 }
